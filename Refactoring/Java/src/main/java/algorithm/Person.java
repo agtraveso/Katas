6f5d8fd78f40eeaf874ded556a744d814c12abab
Alvaro Gomez Traveso <agtraveso@gmail.com>
2022-09-29 10:34:01 +0200

    Transform Person into a java record

--- Refactoring/Java/src/main/java/algorithm/Person.java
+++ Refactoring/Java/src/main/java/algorithm/Person.java
@@ -2,58 +2,18 @@ package algorithm;
 
 import java.util.Date;
 
-public class Person {
-  private final String name;
-  private final Date birthdate;
-
-
-  private Person(String name, Date birthdate) {
-    this.name = name;
-    this.birthdate = birthdate;
-  }
+public record Person(String name, Date birthdate) {
 
   public static Builder builder() {
     return new Builder();
   }
 
-  public String getName() {
-    return name;
-  }
-
-  public Date getBirthdate() {
-    return birthdate;
-  }
-
   public boolean isYoungerThan(Person other) {
-    return this.birthdate.before(other.getBirthdate());
+    return this.birthdate.before(other.birthdate());
   }
 
   public long ageGapWith(Person other) {
-    return this.birthdate.getTime() - other.getBirthdate().getTime();
-  }
-
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
-    }
-
-    if (!(o instanceof Person person)) {
-      return false;
-    }
-
-    return new org.apache.commons.lang3.builder.EqualsBuilder()
-        .append(name, person.name)
-        .append(birthdate, person.birthdate)
-        .isEquals();
-  }
-
-  @Override
-  public int hashCode() {
-    return new org.apache.commons.lang3.builder.HashCodeBuilder(17, 37)
-        .append(name)
-        .append(birthdate)
-        .toHashCode();
+    return this.birthdate.getTime() - other.birthdate().getTime();
   }
 
   public static final class Builder {
@@ -61,7 +21,8 @@ public class Person {
     private String name;
     private Date birthdate;
 
-    private Builder() {}
+    private Builder() {
+    }
 
     public Builder name(String name) {
       this.name = name;
